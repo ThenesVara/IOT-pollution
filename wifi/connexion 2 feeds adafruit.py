a6386def1da4dbea4b1cf6f0b801375ecc439947
LeFameuxSpam <119336198+LeFameuxSpam@users.noreply.github.com>
2022-11-30 17:50:36 +0100

    Create connexion 2 feeds adafruit.py

---
+++ wifi/connexion 2 feeds adafruit.py
@@ -0,0 +1,71 @@
+# This example will send a message to a topic on the Adafruit MQTT broker and then also subscribe to the same topic,
+# in order to show how to use the subscribe functionality.
+
+# Go to your adafruit account : io.adafruit.com/PFE/feeds/temperature
+
+from SI7006A20 import SI7006A20
+from LTR329ALS01 import LTR329ALS01
+from pycoproc_1 import *
+from mqtt import MQTTClient
+from network import WLAN
+import machine
+import time
+
+# led de couleur BLEUE
+pycom.rgbled(0x0080FF)
+
+# initialisation variables pour le capteur de temperature et de lumière
+py = Pycoproc(Pycoproc.PYSENSE)
+dht = SI7006A20(py)
+li = LTR329ALS01(py)
+
+def sub_cb(topic, msg):
+    print(msg)
+
+
+# connexion au routeur
+wlan = WLAN(mode=WLAN.STA)
+wlan.connect("iPhone de Axel", auth=(WLAN.WPA2, "40Gopourtoi"), timeout=5000) #Nom wifi et password
+
+# indique que l'on est connecté au WIFI
+while not wlan.isconnected():
+    machine.idle()
+print("Connected to WiFi\n")
+pycom.rgbled(0x00FF00)
+time.sleep(2)
+
+
+client = MQTTClient("device_id", "io.adafruit.com", user="PFE_Pycom", password="aio_WoPB32roIJPXq6uKkwBlVHePiKiu", port=1883) #votre user adafruit et password
+client1 = MQTTClient("device_id2", "io.adafruit.com", user="PFE_Pycom", password="aio_WoPB32roIJPXq6uKkwBlVHePiKiu", port=1883) #votre user adafruit et password
+
+client.set_callback(sub_cb)
+client.connect()
+client.subscribe(topic="PFE_Pycom/feeds/temperature")
+
+client1.set_callback(sub_cb)
+client1.connect()
+client1.subscribe(topic="PFE_Pycom/feeds/lumiere")
+
+
+# boucle qui envoie les données
+while True:
+    print("Sending Temp")
+    temperature = str("{0:.1f}".format((dht.temperature())))
+    print("la temperature est : ", temperature)
+    client.publish(topic="PFE_Pycom/feeds/temperature", msg=temperature)
+
+    client.check_msg()
+
+    time.sleep(2)
+
+    print("Sending Lux")
+    li_data = li.light()
+    lumiere = str(li_data[0])
+    print("la lumiere est de : ", lumiere)
+    client1.publish(topic="PFE_Pycom/feeds/lumiere", msg=lumiere)
+
+    client1.check_msg()
+
+    pycom.rgbled(0xFF0000)
+    time.sleep(5) # boucle pour envoyer les données toutes les X secondes
+    pycom.rgbled(0x000000) # permet de faire clignoter la led en rouge à chaque envoi de donnée
