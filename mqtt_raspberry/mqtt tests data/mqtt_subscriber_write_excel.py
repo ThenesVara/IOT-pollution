3a8212ef280540c664e822fe28744be8631333b6
ThenesVara <114569016+ThenesVara@users.noreply.github.com>
2023-01-18 15:25:19 +0100

    Add files via upload

--- mqtt_raspberry/mqtt tests data/mqtt_subscriber_write_excel.py
+++ mqtt_raspberry/mqtt tests data/mqtt_subscriber_write_excel.py
@@ -4,21 +4,27 @@ import time
 from datetime import date, datetime
 
 
+path = '/home/thenes/mqtt_raspberry/data.xlsx'
+
+
+'''souscrit aux topics suivants'''
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe([("test1", 1), ("test2", 1), ("topic3", 1)])
+    client.subscribe([("test1", 1), ("test2", 1)])
 
 
 
-
+''' récupère donnee temperature et pollution
+    ecris ces données dans un excel'''
 
 def on_message(client, userdata, message):
     print("Message received: " + message.topic + " : " + str(message.payload))
 
     workbook = Workbook()
+    workbook.save(path)
     worksheet = workbook.active
 
     worksheet.title = 'Data IOT'
@@ -33,20 +39,28 @@ def on_message(client, userdata, message):
     #enregistre les donnees des capteurs par ligne
     donnee_numero = 2 #commence à la deuxieme ligne pour les donnees
     nombre_donnees = 5
+    compteur = 0
 
     while donnee_numero <= nombre_donnees + 2:
+        #date et heure
         today_date = date.today() #date aujourdhui
-
         now = datetime.now() 
         current_time = now.strftime("%H:%M:%S") #heure actuelle
 
-        if message.topic == 'test':
+        #donnees récupérés
+        if message.topic == 'test1':
             i = len(str(message.payload))
             temperature = str(message.payload)[2:i-1]
+            #Topic 3 : temperature
+            worksheet.cell(row=donnee_numero, column=3, value=temperature) 
+            compteur += 1
 
-        if message.topic == 'test':
-            i = len(str(message.payload))
-            pollution = str(message.payload)[2:i-1]
+        if message.topic == 'test2':
+            j = len(str(message.payload))
+            pollution = str(message.payload)[2:j-1]
+            #Topic 4 : pollution
+            worksheet.cell(row=donnee_numero, column=4, value=pollution)  
+            compteur += 1 
 
 
         #Date : colonne 1
@@ -55,57 +69,15 @@ def on_message(client, userdata, message):
         #heure : colonne 2
         worksheet.cell(row=donnee_numero, column=2, value=current_time)
 
-        #Topic 3 : colonne 
-        worksheet.cell(row=donnee_numero, column=3, value=temperature) 
+        #donnee pollution et temperature recupéré -> valeur suivante
+        if compteur>=2:
+            donnee_numero += 1
+            compteur = 0
 
-
-
-        #Topic 4 : colonne 4
-        worksheet.cell(row=donnee_numero, column=4, value=pollution)   
-        donnee_numero += 1
-            
-        workbook.save('/home/thenes/mqtt_connexion/data.xlsx') #enregistre après changement
+        workbook.save(path) #enregistre après changement
 
     #Enregistrement final
-    workbook.save('/home/thenes/mqtt_connexion/tests/data.xlsx') #enregistre après changement
-
-    '''
-    #si ce topic
-    if message.topic == 'test':
-        #dans le fichier ci dessous -> ecris message
-        with open('data.xlsx', 'a+') as f:
-
-
-            f.write(str(message.payload))
-            f.write("\n")'''
-
-def on_message2(client, userdata, message):
-    print("Message received2: " + message.topic + " : " + str(message.payload))
-
-    workbook = Workbook()
-    worksheet = workbook.active
-
-    worksheet.title = 'Data IOT'
-
-
-
-    #enregistre les donnees des capteurs par ligne
-    donnee_numero = 2 #commence à la deuxieme ligne pour les donnees
-    nombre_donnees = 5
-
-    while donnee_numero <= nombre_donnees + 2:
-        
-        if message.topic == 'test2':
-            pollution = str(message.payload)
-        #Topic 4 : colonne 4
-        worksheet.cell(row=donnee_numero, column=4, value=pollution)    
-
-        donnee_numero += 1
-            
-        workbook.save('/home/thenes/mqtt_connexion/data.xlsx') #enregistre après changement
-
-    #Enregistrement final
-    workbook.save('/home/thenes/mqtt_connexion/tests/data.xlsx') #enregistre après changement
+    workbook.save(path) #enregistre après changement
 
 
 
