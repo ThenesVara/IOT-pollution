4052949a720dd6aa824d630e8a89797a04677dfe
ThenesVara <114569016+ThenesVara@users.noreply.github.com>
2023-01-23 22:22:09 +0100

    Update pycom data to adafruit with raspi hotspot.py

--- Raspberry server/Raspap hotpot raspberry/pycom data to adafruit with raspi hotspot.py
+++ Raspberry server/Raspap hotpot raspberry/pycom data to adafruit with raspi hotspot.py
@@ -1,27 +1,41 @@
+''' Raspberry : - en hotspot
+                - connecté en ethernet (ou autre à internet)
+    l'esp32 se connecte au hotspot de la raspberry pour envoyer des données à adafruit
+'''
+
 from mqtt import MQTTClient
 from network import WLAN
 import machine
 import pycom
 import time
 
-def settimeout(duration): pass
+#envoi une valeur fixe
+temperature = '5'
 
-# connexion au Hotspot de la raspberry Pi
+def sub_cb(topic, msg):
+    print(topic,msg)
+
+    
+# Connexion à internet    
 wlan = WLAN(mode=WLAN.STA)
+wlan.connect("RIRraspberry", auth=(WLAN.WPA2, "Raspberrypi"), timeout=5000) #Nom wifi et password
 
-nets = wlan.scan()
-for net in nets:
-    if net.ssid == 'Thenes': #Mettre votre wifi
-        print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, 'thenesoppo'), timeout=5000) #Mettre votre mot de passe
-        #pycom.rgbled(0xff00) 
-        while not wlan.isconnected():
-            machine.idle() # save power while waiting
-        print('WLAN connection succeeded!')
-        break
+while not wlan.isconnected():
+    machine.idle()
+print("Connected to WiFi\n")
 
-client = MQTTClient("joe", "192.168.200.222", port=1883)
-client.settimeout = settimeout
+# MQTT
+client = MQTTClient("device_id", "io.adafruit.com",
+                    user="Thenes", password="aio_EuCI24nsMnvcoWK9tBn2a1Amwokc", port=1883)
+
+client.set_callback(sub_cb) # definition de la fonction de callback pour le topic control
 client.connect()
+client.subscribe(topic="Thenes/feeds/temperature")
 
-client.publish("hello/world", "My publication")
+while True:
+    print("Sending ON")
+    client.publish(topic="Thenes/feeds/temperature", msg=temperature) #message publié au topic indiqué
+    time.sleep(5)
+    print("Sending OFF")
+    client.check_msg()
+    time.sleep(5)
