b3c17ab95b23d6af1ef6eab4a0eb5888ae77bc11
FelixJMB <119596360+FelixJMB@users.noreply.github.com>
2023-02-23 15:00:24 +0100

    Update main.py

--- Cloud/Cloud HiveMQ/main.py
+++ Cloud/Cloud HiveMQ/main.py
@@ -1,1 +1,29 @@
-# main.py -- put your code here!
+from network import WLAN
+from lib.umqtt import MQTTClient
+import machine
+import time
+
+def settimeout(duration): 
+     pass
+
+wlan = WLAN(mode=WLAN.STA)
+wlan.antenna(WLAN.EXT_ANT)
+wlan.connect("OL", auth=(WLAN.WPA2, "trouducul"), timeout=5000)
+
+while not wlan.isconnected(): 
+     machine.idle()
+
+print("Connected to Wifi\n")
+# client = MQTTClient("Felix", "broker.hivemq.com", port=8884)
+client = MQTTClient("Felix", "broker.hivemq.com")
+client.settimeout = settimeout
+# client.disconnect()
+client.connect()
+
+while True:
+     print("Sending ON")
+     client.publish("/lights", "ON")
+     time.sleep(1)
+     print("Sending OFF")
+     client.publish("/lights", "OFF")
+     time.sleep(1)
